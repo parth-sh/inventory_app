4f64e6064640280776ce1ca04186e0f321921357
parth <parth@tradecred.com>
2021-12-18 01:35:55 +0530

    zxing qr code setup

---
+++ app/src/main/java/com/example/inventory_app/CatalogActivity.java
@@ -0,0 +1,46 @@
+package com.example.inventory_app;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
+
+public class CatalogActivity extends AppCompatActivity {
+
+    FloatingActionButton floatingActionButtonScan;
+
+    private final ActivityResultLauncher<ScanOptions> barcodeLauncher = registerForActivityResult(new ScanContract(),
+        result -> {
+            if(result.getContents() == null) {
+                Toast.makeText(CatalogActivity.this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(CatalogActivity.this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+    });
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catalog);
+
+        floatingActionButtonScan = findViewById(R.id.floatingActionButton_scan);
+        floatingActionButtonScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                barcodeLauncher.launch(new ScanOptions()
+//                        .setDesiredBarcodeFormats(ScanOptions.ONE_D_CODE_TYPES)
+                        .setPrompt("Scan a barcode")
+                        .setCameraId(0)
+                        .setBeepEnabled(false)
+                        .setBarcodeImageEnabled(true)
+                );
+            }
+        });
+    }
+}
