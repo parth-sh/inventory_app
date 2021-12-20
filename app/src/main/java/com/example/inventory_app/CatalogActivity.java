ec764997a4298dfa61500b6b6020328f88711d0f
parth <parth@tradecred.com>
2021-12-20 16:01:06 +0530

    Nav bar added

--- app/src/main/java/com/example/inventory_app/CatalogActivity.java
+++ app/src/main/java/com/example/inventory_app/CatalogActivity.java
@@ -1,33 +1,41 @@
 package com.example.inventory_app;
 
 import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
 
 public class CatalogActivity extends AppCompatActivity {
 
-    FloatingActionButton floatingActionButtonScan;
-
     private final ActivityResultLauncher<ScanOptions> barcodeLauncher = registerForActivityResult(new ScanContract(),
-        result -> {
-            if(result.getContents() == null) {
-                Snackbar.make(findViewById(R.id.floatingActionButton_scan), "Cancelled", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            } else {
-                Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
-                intent.putExtra("SCANNED_CODE",result.getContents());
-                startActivity(intent);
-            }
-    });
+            result -> {
+                if (result.getContents() == null) {
+                    Snackbar.make(findViewById(R.id.floatingActionButton_scan), "Cancelled", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                } else {
+                    Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
+                    intent.putExtra("SCANNED_CODE", result.getContents());
+                    startActivity(intent);
+                }
+            });
+    FloatingActionButton floatingActionButtonScan;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,5 +55,40 @@ public class CatalogActivity extends AppCompatActivity {
                 );
             }
         });
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.main_drawer_open, R.string.main_drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                drawerLayout.closeDrawer(GravityCompat.START);
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        Toast.makeText(getApplicationContext(),"Home",Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.nav_gallery:
+                        Toast.makeText(getApplicationContext(),"Gallery",Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.nav_slideshow:
+                        Toast.makeText(getApplicationContext(),"Slideshow",Toast.LENGTH_SHORT).show();
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
